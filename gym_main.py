e4a3993282d9e47ab147323cee6773d9f8dda7a4
AlwaysOMG <alan135790.iem06@nctu.edu.tw>
2022-12-29 01:58:16 +0800

    train and test

--- gym_main.py
+++ gym_main.py
@@ -1,14 +1,26 @@
-import numpy as np
+# import packages
+import datetime
+import time
 import gym
+
+# pip install tensorboard
+# pip install torch-tb-profiler
+# tensorboard --logdir='log'
+from torch.utils.tensorboard import SummaryWriter
 
 # pip install stable-baselines3
 from stable_baselines3 import PPO
 
-env = gym.make('TSMStock-v1')
+# setting
+algorithm_name = 'PPO'
+train_epochs = 1000*100
 
-model = PPO("MlpPolicy", env, verbose=1)
-model.learn(total_timesteps=25000)
-model.save("ppo_cartpole")
+# train PPO algorithm
+env = gym.make('TSMStock-v1')
+model = PPO('MlpPolicy', env, n_steps=8, batch_size=8, \
+    tensorboard_log=f"log/{algorithm_name}_{datetime.date.today()}_{time.time()}")
+model.learn(total_timesteps=250*train_epochs)
+model.save('ppo_model')
 
 obs = env.reset()
 done = False
