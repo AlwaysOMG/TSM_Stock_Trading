53ec1ce1807c10501aa2f31eb224fa8bfe00a93e
AlwaysOMG <alan135790.iem06@nctu.edu.tw>
2022-12-29 23:56:46 +0800

    change model to dqn

--- gym_main.py
+++ gym_main.py
@@ -5,26 +5,32 @@ import gym
 
 # pip install tensorboard
 # pip install torch-tb-profiler
-# tensorboard --logdir='log'
+# tensorboard --logdir='log/stable-baselines3'
 from torch.utils.tensorboard import SummaryWriter
 
 # pip install stable-baselines3
 from stable_baselines3 import PPO
+from stable_baselines3 import DQN
 
 # setting
-algorithm_name = 'PPO'
+algorithm_name = 'DQN'
 train_epochs = 1000*100
 
 # train PPO algorithm
 env = gym.make('TSMStock-v1')
-model = PPO('MlpPolicy', env, n_steps=8, batch_size=8, \
+"""
+PPO_model = PPO('MlpPolicy', env, n_steps=8, batch_size=8, \
     tensorboard_log=f"log/{algorithm_name}_{datetime.date.today()}_{time.time()}")
-model.learn(total_timesteps=250*train_epochs)
-model.save('ppo_model')
+PPO_model.learn(total_timesteps=250*train_epochs)
+PPO_model.save('ppo_model')
+"""
+DQN_model = DQN("MlpPolicy", env, exploration_fraction=0.4, \
+    tensorboard_log=f"log/stable-baselines3/{algorithm_name}_{datetime.date.today()}_{time.time()}")
+DQN_model.learn(total_timesteps=250*train_epochs)
 
 obs = env.reset()
 done = False
 while not done:
-    action, _states = model.predict(obs)
+    action, _states = DQN_model.predict(obs)
     obs, rewards, done, info = env.step(action)
     env.render()
