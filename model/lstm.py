8a8aa12503aec5e29a97699729110fb6508b11a5
AlwaysOMG <alan135790.iem06@nctu.edu.tw>
2022-12-29 14:07:07 +0800

    lstm

---
+++ model/lstm.py
@@ -0,0 +1,7 @@
+# import packages
+import numpy as np
+
+# pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
